b83d80a9dcc48fbe292f6fd44f3c3dcb79dcd3dd
Muhammad Miqdad Ghifar <mmiqdad76@gmail.com>
2020-10-27 22:45:28 +0700

    Add project files.

---
+++ ClientCFC-P4-041-Form/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ServiceModel;
+using ServiceMtk_P2_20180140041;
+
+namespace ClientCFC_P4_041_Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Enabled = false;
+            label2.Enabled = false;
+            label3.Enabled = false;
+            label4.Enabled = false;
+
+
+            BasicHttpBinding bind = new BasicHttpBinding();
+            ChannelFactory<IMatematika> objChannel = new ChannelFactory<IMatematika>(bind, "http://localhost:1907");
+            IMatematika obj = objChannel.CreateChannel();
+            double hasilTambah = obj.Tambah(1, 2);
+            Console.WriteLine("1 + 2 = " + hasilTambah);
+            double hasilKurang = obj.Kurang(3, 2);
+            Console.WriteLine("3 - 2 = " + hasilKurang);
+            double hasilKali = obj.Kali(2, 2);
+            Console.WriteLine("2 x 2 = " + hasilKali);
+            double hasilBagi = obj.Bagi(2, 2);
+            Console.WriteLine("2 / 2 = " + hasilBagi);
+
+
+            label1.Text = "1 + 2 = " + hasilTambah.ToString();
+            label2.Text = "3 - 2 = " + hasilKurang.ToString();
+            label3.Text = "2 x 2 = " + hasilKali.ToString();
+            label4.Text = "2 / 2 = " + hasilBagi.ToString();
+        }
+    }
+}
